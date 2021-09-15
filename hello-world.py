1453ce462306f90db14c997b6e20b72a9cf552f1
ColdMoonNight <pixelnitroboost@gmail.com>
2021-09-15 18:24:59 -0500

    yay adding more stuff

---
+++ hello-world.py
@@ -0,0 +1,5 @@
+print('Hello World')
+
+#introduces the "print()" function. This function requires the text you want to print in the `` inside of the () that appends the print
+
+#expected output of code ~ Hello World
