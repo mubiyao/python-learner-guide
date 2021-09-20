49632e3dad63c55717b3e2b2a5048e0bebf0216f
Sashi-boi <90008981+Sashi-boi@users.noreply.github.com>
2021-09-20 18:18:45 -0500

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -2,6 +2,7 @@ print('Hello World')
 
 # introduces the "print()" function. This function requires the text you want to print in the `` inside of the () that appends the print
 
-# expected output of code ~ Hello World
+# expected output of code ~ Hello World, you can also use any word or phrase you want to print inside of the quotation marks.
 
 # oh, and you can also use a hastag to write comments, which give insight on the code you right and don't affect the actual code you write. Very helpful for beginners
+
