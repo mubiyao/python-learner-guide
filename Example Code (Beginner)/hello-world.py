8aae96542d122bc816307c74e219b3ed10318172
Sashi-boi <90008981+Sashi-boi@users.noreply.github.com>
2021-09-25 18:52:27 -0500

    Create hello-world.py

---
+++ Example Code (Beginner)/hello-world.py
@@ -0,0 +1,7 @@
+print('Hello World')
+
+# introduces the "print()" function. This function requires the text you want to print in the `` inside of the () that appends the print
+
+# expected output of code ~ Hello World
+
+# oh, and you can also use a hastag to write comments, which give insight on the code you right and don't affect the actual code you write. Very helpful for starters.
