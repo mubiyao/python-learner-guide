b7170f8ed35de9cbc7f0b1cea1c320a4180e7c01
Sashi-boi <90008981+Sashi-boi@users.noreply.github.com>
2021-09-25 17:47:28 -0500

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -4,4 +4,4 @@ print('Hello World')
 
 # expected output of code ~ Hello World
 
-# oh, and you can also use a hastag to write comments, which give insight on the code you right and don't affect the actual code you write. Very helpful for beginners
+# oh, and you can also use a hastag to write comments, which give insight on the code you right and don't affect the actual code you write. Very helpful for starters.
