8645c7ba2b072521fd0a037f056914c19d8aed39
partlyaimboy <91577395+lilaimboy@users.noreply.github.com>
2021-09-29 12:14:16 -0500

    Update dice-simulator.py
    less cronge

--- dice-simulator.py
+++ dice-simulator.py
@@ -1,11 +1,11 @@
-import random # Imports the random module in case you couldn't tell.
+import random # imports radom module, which allows for the random methods/functions later-on
 
-again = True
+again = True # i honestly dont know why..... but just substititue a "True" call for "again" in the code
 
-while again:
-    print(random.randint(1, 6)) # Prints a random number from 1 to 6 like on a dice.
-    another_roll = input("Want to roll the dice again? (y/n): ") # Gives the user a choice to roll again which the choices being 'y' for yes and 'n' for no.
-    if another_roll.lower() == "y": # If the user inputs "y", repeats the statement and generates another random number from 1 to 6.
-        continue # Continues the statment as said in previous line.
-    else: # If the user inputs 'n' or anything else, brings the code to a stop.
-        break # Ends the program :)
+while again: # while "again" runs:
+    print(random.randint(1, 6)) # the program will print a random number from 1-6
+    another_roll = input("Want to roll the dice again? (y/n): ") # self explanatory text, asks "y/n" to continue the rolling
+    if another_roll.lower() == "y": # if y is pressen then...
+        continue # the program continues
+    else: # any other key bress will result in...
+        break # the program ending.
