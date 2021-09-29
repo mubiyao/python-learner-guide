7708cf364d3c242e0f4c09e61ad380579db4a562
partlyaimboy <91577395+lilaimboy@users.noreply.github.com>
2021-09-29 12:24:05 -0500

    Update odds-and-evens-game.py
    i honestly am wating time rn

--- odds-and-evens-game.py
+++ odds-and-evens-game.py
@@ -1,22 +1,22 @@
-import random # imports the random module
+import random # random module used AGAIN!
 
-def even_odd(choice, fingers): # choice is whether its even or odd and fingers is the number
-    human_choice = choice # human_choice is whether its odd or even.
-    human_fingers = fingers # human_fingers is what number they chose from 1 to 10.
-    if human_choice == "Even": # if the human choice is even, the computer choice must be odd.
+def even_odd(choice, fingers): # defines choice and fingers
+    human_choice = choice # human choice will be substitued for choice in the rest fo the program
+    human_fingers = fingers # i.... really don't know
+    if human_choice == "Even": # if the human chooses even, the computer chooses odd
         computer_choice = "Odd"
     else:
-        computer_choice = "Even" # if the human choice is odd, the computer choice must be even.
-    computer_finger = random.randint(0,10) # the computers choice of what number it will be. Could be odd or even ranging from 1 to 10 depending on what the player picked.
+        computer_choice = "Even" # if the human choice is odd, the computer chooses even
+    computer_finger = random.randint(0,10) # computer chooses what number using a randint generator
     total = human_fingers + computer_finger # the winner is decided by the total of the computers fingers and the players fingers.
-    if total % 2 ==0: # results are even if statement appears true.
+    if total % 2 ==0: # if the total is divisible by two, its even (basic math rule)
         results = "Even"
     else:
-        results = "Odd" # results are odd if the ststement above appears to be something else. 
-    if human_choice == results: # if the human choice is even, prints 'You won' as below.
-        print ("Yay! You won! :D")
-    else: # if the human choice is something other than even, prints 'You lost' as below.
-        print ("You lost. The computer wins. :(")
+        results = "Odd" # if its anything else (not divisible by 2) then it is odd
+    if human_choice == results: # if human wins, print winning message
+        print ("You Won! c:")
+    else: # if anything other than a win, then print loss message
+        print ("You Lose... The computer outsmarted you :c")
         
 
-even_odd("Odd", 5) # where the player inputs their choices for each round.
+even_odd("Odd", 5) # round input
